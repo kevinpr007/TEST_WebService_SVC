9816f4409a35a8b919e6b76508dfdce4a1fc0c68
Kevin Rivera <kevinpr_007@hotmail.com>
2020-02-12 13:18:15 -0400

    Testing the Web Service SVC

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReferenceTest.Service1Client service1Client = new ServiceReferenceTest.Service1Client();
+            Console.WriteLine(String.Format("Received from Web Service: {0}", service1Client.sum(1, 4)));
+            Console.ReadLine();
+        }
+    }
+}
